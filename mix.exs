3b44a1985c3ab0c2f4378d92c468b5598de417e6
Tallak Tveide <tallakt@Tallaks-iMac.local>
2015-10-26 07:30:17 +0100

    Version bump 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Codepagex.Mixfile do
 
   def project do
     [app: :codepagex,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      name: "Codepagex",
      description: description,
