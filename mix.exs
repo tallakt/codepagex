b522a5e8aa89b4c01b567306c1029b0bc527ab62
Tallak Tveide <tallak@tveide.net>
2016-06-20 16:30:43 +0200

    Version 1.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Codepagex.Mixfile do
 
   def project do
     [app: :codepagex,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.0",
      name: "Codepagex",
      description: description,
