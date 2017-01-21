139a2b94fbe2368502273f4322e42a084d261fc7
Po Chen <chenpaul914@gmail.com>
2017-01-21 22:56:42 +1100

    Update mappings.ex

--- lib/codepagex/mappings.ex
+++ lib/codepagex/mappings.ex
@@ -160,7 +160,7 @@ defmodule Codepagex.Mappings do
 
   # These are documented in Codepagex.encoding_list/1
   def encoding_list(selection \\ nil)
-  def encoding_list(:all), do: @all_names_files |> Keyword.keys |> Enum.sort
+  def encoding_list(:all), do: @all_names_files |> Map.keys |> Enum.sort
   def encoding_list(_), do: @filtered_names_files |> Keyword.keys |> Enum.sort
 
   # load mapping files
