4cddd311849a3eb471985f02ca73e51b11345a5d
Tallak Tveide <tt@kitemill.no>
2020-09-27 15:20:02 +0200

    Bump to version 0.1.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Codepagex.Mixfile do
 
   def project do
     [app: :codepagex,
-     version: "0.1.4",
+     version: "0.1.5",
      elixir: "~> 1.5",
      name: "Codepagex",
      description: description(),
